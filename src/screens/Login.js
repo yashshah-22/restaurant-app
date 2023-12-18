d6afd972535652b7873d6dcd714485d5052e1ffd
yashshah-22 <105560379+yashshah-22@users.noreply.github.com>
2023-12-19 00:54:39 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/loginuser', {
+    const response = await fetch('restaurant-app-mernstack-22.vercel.app/api/loginuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
