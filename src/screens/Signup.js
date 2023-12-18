b2dcc899246195bf92f382bdf4f26ed0bc8b4fa6
yashshah-22 <105560379+yashshah-22@users.noreply.github.com>
2023-12-19 00:55:21 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -9,7 +9,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/createuser', {
+    const response = await fetch('restaurant-app-mernstack-22.vercel.app/api/createuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
